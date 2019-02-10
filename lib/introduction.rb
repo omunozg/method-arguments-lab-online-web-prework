a6ac839e311fce6e29e36f4eb7d10975a4c15e13
Oscar Munoz <omunozg@emory.edu>
2019-02-10 21:12:25 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,7 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} an I am learning to program in #{language}"
+end
